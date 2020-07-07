da3e282f71a9da9aba42e4d373875aa00e44c82b
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-07-08 01:36:05 +0300

    Add Docstrings to the methods.

--- stack.py
+++ stack.py
@@ -1,16 +1,28 @@
 class Stack:
     def __init__(self):
+        """
+            This method contains the instantiation of the Stack as a list.
+        """
         self.stack_items = []
 
     def push(self, item):
+        """
+            In this method we add items to the top of the stack. This method returns nothing, it just adds the given element to the Stack.
+        """
         self.stack_items.append(item)
 
     def pop(self):
+        """
+            In this method we remove the top most item off the stack if the Stack is not empty. This method returns the top most item that has been removed or False if the Stack is empty.
+        """
         if self.stack_items:
             return self.stack_items.pop()
         return False
 
     def peek(self):
+        """
+            This method enables us to be able to see what element is at the top of the stack therego this is the element to be popped if need be. The stack must be non-empty for this to work, if it isn't the method returns False.
+        """
         if self.stack_items:
             return self.stack_items[-1]
         return False
