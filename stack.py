d56484b8acbedf5a0b776833f121de10897f0684
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-07-08 01:24:44 +0300

    Create the push method.

--- stack.py
+++ stack.py
@@ -1,9 +1,10 @@
 class Stack:
     def __init__(self):
         self.stack_items = []
-        
+
     def push(self, item):
-        pass
+        self.stack_items.append(item)
+        
     def pop(self):
         pass
     def peek(self):
